0fed46795693a2754e89be9e7dbefadfef21fd01
Ramesh Babu <mr.vrbabu@hotmail.com>
2020-07-26 02:02:30 +0530

    adding webserver with different port

--- Deploy-single-server/main.tf
+++ Deploy-single-server/main.tf
@@ -1,11 +1,37 @@
 provider "aws" {
-  region = "us-east-1"
+  profile = "default"
+  region  = "us-east-1"
 }
 
-resource "aws_instance" "myterraformvm01" {
-  ami           = "ami-0ac80df6eff0e70b5"
-  instance_type = "t2.micro"
+resource "aws_instance" "ubuntu" {
+  ami                    = "ami-0ac80df6eff0e70b5"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.my_upnrun_8081.id]
+  user_data              = <<-EOF
+              #!/bin/bash
+              echo "Hello, Welcome to the world of terraform....." > index.html
+              nohup busybox httpd -f -p 8081 &
+              EOF
+
+
   tags = {
-    Name = "myterraform_vm01"
+    Name = "upnrunning_vm01"
   }
+}
+
+resource "aws_security_group" "my_upnrun_8081" {
+  name = "my_upnrun_8081"
+
+  ingress {
+    description = "Webserver port"
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.ubuntu.id
 }
